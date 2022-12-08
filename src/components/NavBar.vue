27c6c5bf396c1296b9248891b057dc61b5d5e357
joaovsa97 <joaos.azevedo10@gmail.com>
2022-12-08 11:39:49 -0300

    App Created

---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-dark sticky-top">
+    <div class="container-fluid">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div
+        class="collapse justify-content-between navbar-collapse"
+        id="navbarNav"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item m-2">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item m-2">
+            <router-link to="/add">Add</router-link>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+ul,
+a {
+  list-style: none;
+  text-decoration: none;
+  color: white;
+}
+
+img {
+  width: 40%;
+  object-fit: contain;
+}
+</style>
---
+++ src/views/Categories/AddCategory.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container col-md-6 mt-5">
+    <div class="row">
+      <div class="text-center">
+        <h3>Add Category</h3>
+      </div>
+    </div>
+    <div class="row">
+      <form class="text-center">
+        <div class="form-group m-3">
+          <label>Category Name</label>
+          <input type="text" class="form-control" v-model="categoryName" />
+        </div>
+        <div class="form-group m-3">
+          <label>Category Description</label>
+          <textarea type="text" class="form-control" v-model="description" />
+        </div>
+        <div class="form-group m-3">
+          <label>Category Image</label>
+          <input type="text" class="form-control" v-model="imageUrl" />
+        </div>
+        <button type="submit" class="btn btn-success">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      categoryName: "",
+      description: "",
+      imageUrl: "",
+    };
+  },
+  methods: {
+    addCategory() {},
+  },
+};
+</script>
+<style scoped></style>
